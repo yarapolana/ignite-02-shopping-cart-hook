39a43468d2398d5e3f47bf927979d111a73dd016
Yara Polana <yara.polana@gmail.com>
2021-04-15 09:56:44 +0000

    update to mozambican custom currency format

--- src/util/format.ts
+++ src/util/format.ts
@@ -1,4 +1,6 @@
-export const { format: formatPrice } = new Intl.NumberFormat('pt-br', {
-  style: 'currency',
-  currency: 'BRL',
-});
+export function formatCurrency(amount: number) {
+  return new Intl.NumberFormat('de-DE', {
+    currency: 'MZN',
+    style: 'currency',
+  }).format(amount).replace('MZN', 'Mts')
+}
