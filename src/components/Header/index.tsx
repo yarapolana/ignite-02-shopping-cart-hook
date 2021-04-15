37e02c33127aa3d49fab69bd442ae2bf9160b2fa
Yara Polana <yara.polana@gmail.com>
2021-04-15 17:15:23 +0000

    add cartsize

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,8 +6,8 @@ import { Container, Cart } from './styles'
 import { useCart } from '../../contexts/useCart'
 
 export const Header = (): JSX.Element => {
-  // const { cart } = useCart()
-  // const cartSize = // TODO
+  const { cart } = useCart()
+  const cartSize = cart.length
 
   return (
     <Container>
@@ -19,7 +19,7 @@ export const Header = (): JSX.Element => {
         <div>
           <strong>Meu carrinho</strong>
           <span data-testid="cart-size">
-            {/* {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`} */}
+          ${cartSize} {cartSize === 1 ? 'item' : 'itens'}
           </span>
         </div>
         <MdShoppingBasket size={36} color="#FFF" />
